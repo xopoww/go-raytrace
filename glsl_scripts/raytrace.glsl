c6a3cb606953323afda2bff10aae1dbc6174219b
Arseny Khoroshilov <horoshilov.aa@phystech.edu>
2021-03-30 01:38:17 +0300

    add sphere support in raytracer

--- glsl_scripts/raytrace.glsl
+++ glsl_scripts/raytrace.glsl
@@ -15,12 +15,22 @@ struct box {
   vec3 max;
 };
 
+struct ball {
+  vec3 center;
+  float radius;
+};
+
 #define NUM_BOXES 2
 const box boxes[] = {
   /* The ground */
   {vec3(-5.0, -0.1, -5.0), vec3(5.0, 0.0, 5.0)},
   /* Box in the middle */
   {vec3(-0.5, 0.0, -0.5), vec3(0.5, 1.0, 0.5)}
+};
+
+#define NUM_BALLS 1
+const ball balls[] = {
+  {vec3(-2.0, 0.7, 1), 0.7}
 };
 
 
@@ -34,22 +44,55 @@ vec2 intersectBox(vec3 origin, vec3 dir, const box b) {
   return vec2(tNear, tFar);
 }
 
+vec2 solve_quadratic(float a, float b, float c) {
+  if (a == 0.0) {
+    float k = -b / c;
+    return vec2(k, k);
+  }
+  float D2 = b * b - 4 * a * c;
+  if (D2 < 0.0) {
+    return vec2(1.0 / 0.0, 1.0 / 0.0);
+  }
+  float x = -b / 2.0 / a;
+  if (D2 == 0.0) {
+    return vec2(x, x);
+  }
+  float delta = sqrt(D2) / 2.0 / a;
+  return vec2(x - delta, x + delta);
+}
+
+vec2 intersectBall(vec3 origin, vec3 dir, const ball b) {
+  float c1 = pow(length(dir), 2);
+  float c2 = 2.0 * dot(origin - b.center, dir);
+  float c3 = pow(length(origin - b.center), 2) - pow(b.radius, 2);
+  return solve_quadratic(c1, c2, c3);
+}
+
 
 #define MAX_SCENE_BOUNDS 1000.0
 
 struct hitinfo {
   vec2 lambda;
-  int bi;
+  int oi;
 };
 
-bool intersectBoxes(vec3 origin, vec3 dir, out hitinfo info) {
+bool intersectObjects(vec3 origin, vec3 dir, out hitinfo info) {
   float smallest = MAX_SCENE_BOUNDS;
   bool found = false;
   for (int i = 0; i < NUM_BOXES; i++) {
     vec2 lambda = intersectBox(origin, dir, boxes[i]);
     if (lambda.x > 0.0 && lambda.x < lambda.y && lambda.x < smallest) {
       info.lambda = lambda;
-      info.bi = i;
+      info.oi = i;
+      smallest = lambda.x;
+      found = true;
+    }
+  }
+  for (int i = 0; i < NUM_BALLS; i++) {
+    vec2 lambda = intersectBall(origin, dir, balls[i]);
+    if (lambda.x > 0.0 && lambda.x < lambda.y && lambda.x < smallest) {
+      info.lambda = lambda;
+      info.oi = i + NUM_BOXES;
       smallest = lambda.x;
       found = true;
     }
@@ -60,8 +103,8 @@ bool intersectBoxes(vec3 origin, vec3 dir, out hitinfo info) {
 
 vec4 trace(vec3 origin, vec3 dir) {
   hitinfo i;
-  if (intersectBoxes(origin, dir, i)) {
-    vec4 gray = vec4(i.bi / 10.0 + 0.8);
+  if (intersectObjects(origin, dir, i)) {
+    vec4 gray = vec4(i.oi / 10.0 + 0.6);
     return vec4(gray.rgb, 1.0);
   }
   return vec4(0.0, 0.0, 0.0, 1.0);
